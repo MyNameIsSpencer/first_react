a152215cc0e33d04fef1580b7a67d37e1ece16f7
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-03 01:39:36 -0500

    first React commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './App'
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+)
+// registerServiceWorker();
